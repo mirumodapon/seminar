58d7666b5a720343f679fa1c5a889f2aa51f510e
Mirumo <mysteryperson520@gmail.com>
2026-01-05 21:38:50 +0800

    feat: add config module

--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,9 +1,12 @@
+import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
-  await app.listen(process.env.PORT ?? 3000)
+  const config = app.get(ConfigService)
+
+  await app.listen(config.get('app.port')!)
 }
 
 bootstrap()
