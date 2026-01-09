45ec867b564680d91cb8b1018ef16352357fccd1
Mirumo <mysteryperson520@gmail.com>
2026-01-09 21:30:36 +0800

    feat: setup session

--- apps/backend/src/config/config.factory.ts
+++ apps/backend/src/config/config.factory.ts
@@ -3,6 +3,15 @@ import { registerAs } from '@nestjs/config'
 export default [
   registerAs('app', () => ({
     port: process.env.PORT || 3000,
+    session: {
+      secret: process.env.SESSION_SECRET,
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        secure: false,
+        maxAge: 24 * 60 * 60 * 1000,
+      },
+    },
   })),
   registerAs('database', () => ({
     knex: {
--- apps/backend/src/main.ts
+++ apps/backend/src/main.ts
@@ -1,11 +1,20 @@
 import { ValidationPipe } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { NestFactory } from '@nestjs/core'
+import { RedisStore } from 'connect-redis'
+import session, { SessionOptions } from 'express-session'
 import { AppModule } from './app.module'
+import { REDIS_PROVIDER } from './database/redis/redis.constant'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
   const config = app.get(ConfigService)
+  const redis = app.get(REDIS_PROVIDER)
+
+  app.use(session({
+    ...config.get<SessionOptions>('app.session', { secret: '' }),
+    store: new RedisStore({ client: redis }),
+  }))
 
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
