8260bf020872b97444eaa6670c8b15d5d32e50e0
Mirumo <mysteryperson520@gmail.com>
2026-01-09 21:59:23 +0800

    fix(config): set session cookie secure only in production

--- apps/backend/src/config/config.factory.ts
+++ apps/backend/src/config/config.factory.ts
@@ -8,7 +8,7 @@ export default [
       resave: false,
       saveUninitialized: false,
       cookie: {
-        secure: false,
+        secure: process.env.NODE_ENV === 'production',
         maxAge: 24 * 60 * 60 * 1000,
       },
     },
