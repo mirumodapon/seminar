2c6ff07ed4290db31e5931f319ad4322101d4c63
Mirumo <mysteryperson520@gmail.com>
2026-01-17 13:45:47 +0800

    feat(activity): implement page CURD api

--- apps/backend/src/activity/activity.controller.ts
+++ apps/backend/src/activity/activity.controller.ts
@@ -1,11 +1,17 @@
 import { Body, ConflictException, Controller, Delete, Get, HttpCode, HttpStatus, NotFoundException, Param, Patch, Post } from '@nestjs/common'
 import { ActivityService } from './activity.service'
 import { CreateActivityDto } from './dto/create-activity.dto'
+import { CreatePageDto } from './dto/create-page.dto'
 import { UpdateActivityDto } from './dto/update-activity.dto'
+import { UpdatePageDto } from './dto/update-page.dto'
+import { PageService } from './page.service'
 
 @Controller('activity')
 export class ActivityController {
-  constructor(private readonly activityService: ActivityService) { }
+  constructor(
+    private readonly activityService: ActivityService,
+    private readonly pageService: PageService,
+  ) { }
 
   @Post()
   async createActivity(@Body() createActivityDto: CreateActivityDto) {
@@ -47,5 +53,67 @@ export class ActivityController {
   @HttpCode(HttpStatus.NO_CONTENT)
   async deleteActivity(@Param('id') activityId: string) {
     await this.activityService.deleteActivity(activityId)
+  }
+
+  @Post(':activityId/page')
+  async createPage(
+    @Param('activityId') activityId: string, @Body()
+    createPageDto: CreatePageDto,
+  ) {
+    const activity = await this.activityService.findActivityById(activityId)
+
+    if (!activity) {
+      throw new NotFoundException('Activity not found')
+    }
+
+    const existingPage = await this.pageService.findPageById(activityId, createPageDto.pageId)
+
+    if (existingPage) {
+      throw new ConflictException('Page already exists')
+    }
+
+    return this.pageService.createPage(activityId, createPageDto)
+  }
+
+  @Get(':activityId/page/:pageId')
+  async findPageById(
+    @Param('activityId') activityId: string,
+    @Param('pageId') pageId: string,
+  ) {
+    const page = await this.pageService.findPageById(activityId, pageId)
+
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
+
+    return page
+  }
+
+  @Patch(':activityId/page/:pageId')
+  async updatePage(
+    @Param('activityId') activityId: string,
+    @Param('pageId') pageId: string,
+    @Body() updatePageDto: UpdatePageDto,
+  ) {
+    const page = await this.pageService.updatePage(activityId, pageId, updatePageDto)
+
+    if (!page) {
+      throw new NotFoundException('Page not found')
+    }
+
+    return page
+  }
+
+  @Delete(':activityId/page/:pageId')
+  @HttpCode(HttpStatus.NO_CONTENT)
+  async deletePage(
+    @Param('activityId') activityId: string,
+    @Param('pageId') pageId: string,
+  ) {
+    const affect = await this.pageService.deletePage(activityId, pageId)
+
+    if (!affect) {
+      throw new NotFoundException('Page not found')
+    }
   }
 }
