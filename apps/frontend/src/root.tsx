10ba84e7da94271283493a06eeafa58cf5fee072
Mirumo <mysteryperson520@gmail.com>
2026-02-24 23:48:32 +0800

    chore: add easter egg

--- apps/frontend/src/root.tsx
+++ apps/frontend/src/root.tsx
@@ -1,5 +1,4 @@
 import type { Route } from '../app/+types/root'
-
 import {
   isRouteErrorResponse,
   Links,
@@ -8,6 +7,7 @@ import {
   Scripts,
   ScrollRestoration,
 } from 'react-router'
+import useEasterEgg from './hooks/useEasterEgg'
 
 import './tailwind.css'
 
@@ -33,6 +33,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
+  useEasterEgg()
   return <Outlet />
 }
 
